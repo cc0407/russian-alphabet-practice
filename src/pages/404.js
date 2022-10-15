678da49621dcfecd39aaa455b248ffa400f537eb
cc0407 <cc0407@live.ca>
2022-10-15 18:03:07 -0400

    updated 404
    matched styling of current site on 404
    Removed unused components

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,25 +1,19 @@
 import React from "react";
-import Nav from "../components/Nav";
-import RoundedLink from "../components/RoundedLink";
-import Footer from "../components/Footer";
 import Head from "../components/Head";
 
 const pageNotFound = () => {
     return(
-        <div className="column min-h-screen">
+        <div className="h-full w-full bg-lightBlue column centered overflow-hidden">
             <Head 
                 title="Page Not Found..."
                 description=""
             />
-            <Nav />
-            <div className="column centered bg-darkBlue flex-grow-default">
-                <div className="title font-bold text-white">Oops...</div>
-                <div className="subtitle text-white">
-                The page you are looking for does not exist.
+            <div className="midground">
+                <div className="foreground column centered">
+                    <div className="title font-bold">Oops...</div>
+                    <div>The page you are looking for does not exist.</div>
                 </div>
-                <RoundedLink title="Back to Home" link="/" target="_self" />
             </div>
-            <Footer homepage={true}/>
         </div>
     );
 }
