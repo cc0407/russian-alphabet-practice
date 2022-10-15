e90d04c3360e7e7847371f5a08a9b65ffd08cc05
cc0407 <cc0407@live.ca>
2022-10-15 16:12:36 -0400

    pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,5 +101,5 @@ module.exports = {
   ],
   
   //Change REPONAME to the name of this repo. Necessary for gh-pages to function
-  pathPrefix: "/cyrillic-test",
+  pathPrefix: "/russian-alphabet-practice",
 };
